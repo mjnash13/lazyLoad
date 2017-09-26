1968981c12dd71bf96334d3645f3499d7e117466
秦子健 <qinzj13@gmail.com>
2017-09-26 17:34:10 +0800

    节流优化

--- lazyLoad.js
+++ lazyLoad.js
@@ -1,18 +1,28 @@
-var images = document.getElementsByClassName('lazyLoad');//需要延迟加载的图片class为'lazyLoad'，获取需要延迟加载的图片。
+var images = document.getElementsByClassName('lazyLoad'); //需要延迟加载的图片class为'lazyLoad'，获取需要延迟加载的图片。
 
-lazyLoad(images);//页面初始化时先执行一次，把需要加载的图片先加载。
+lazyLoad(images); //页面初始化时先执行一次，把需要加载的图片先加载。
 
+//函数节流
+var canRun = true;
 window.onscroll = function() {
-    lazyLoad(images);
+    if (!canRun) {
+        return;
+    }
+    canRun = false;
+    setTimeout(function() {
+        lazyLoad(images);
+        canRun = true;
+    }, 300);
 };
 //窗口滚动时触发加载函数。
 
 function lazyLoad(images) {
     for (var i = 0; i < images.length; i++) {
         if (!images[i].isLoad && images[i].getBoundingClientRect().top < document.documentElement.clientHeight) {
-            images[i].isLoad = true;//更改标志位，已经加载的图片就不再加载。
+            images[i].isLoad = true; //更改标志位，已经加载的图片就不再加载。
             /*images[i].src = images[i].getAttribute('data-src');*/
             loadImage(images[i]);
+            document.getElementById("text").innerHTML += "<div>第" + i + "张图片加载了</div>";
         }
     }
 }
